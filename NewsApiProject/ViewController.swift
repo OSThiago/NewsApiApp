e0bfdc84ad8f6e218da143616a71f5b8535b67c3
Thiago <52988009+OSThiago@users.noreply.github.com>
2024-05-19 12:47:50 -0300

    feat: add project

---
+++ NewsApiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApiProject
+//
+//  Created by Thiago de Oliveira Sousa on 19/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
